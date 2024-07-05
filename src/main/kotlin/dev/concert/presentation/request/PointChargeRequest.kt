c45ae4d67550860479c29e1ca92e31d1ab196d85
juhwan <asdcz@naver.com>
2024-07-05 22:53:11 +0900

    포인트 충전 조회 request dto 구현

--- src/main/kotlin/dev/concert/presentation/request/PointChargeRequest.kt
+++ src/main/kotlin/dev/concert/presentation/request/PointChargeRequest.kt
@@ -1,6 +1,13 @@
 package dev.concert.presentation.request
 
+import dev.concert.application.point.dto.PointRequestDto
+
 data class PointChargeRequest(
     val userId : Long,
     val amount : Long,
+)
+
+fun PointChargeRequest.toDto() = PointRequestDto (
+    userId = userId,
+    amount = amount,
 )
