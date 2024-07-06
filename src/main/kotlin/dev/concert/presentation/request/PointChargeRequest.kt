04604fcede9d83ced4286cf09ddd13e3a2221164
juhwan <asdcz@naver.com>
2024-07-06 11:30:09 +0900

    스웨거 설정 추가

--- src/main/kotlin/dev/concert/presentation/request/PointChargeRequest.kt
+++ src/main/kotlin/dev/concert/presentation/request/PointChargeRequest.kt
@@ -1,9 +1,15 @@
 package dev.concert.presentation.request
 
 import dev.concert.application.point.dto.PointRequestDto
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(description = "포인트 충전 요청")
 data class PointChargeRequest(
+
+    @Schema(description = "사용자 ID", example = "1")
     val userId : Long,
+
+    @Schema(description = "충전할 포인트 금액", example = "1000")
     val amount : Long,
 )
 
