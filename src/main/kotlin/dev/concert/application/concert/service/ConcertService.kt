3fe63ef6a4babf22f85001de39374136dde056d7
juhwan <asdcz@naver.com>
2024-07-07 20:15:15 +0900

    콘서트 날짜 조회 비즈니스 로직 인터페이스 구현

--- src/main/kotlin/dev/concert/application/concert/service/ConcertService.kt
+++ src/main/kotlin/dev/concert/application/concert/service/ConcertService.kt
@@ -1,7 +1,9 @@
 package dev.concert.application.concert.service
 
+import dev.concert.application.concert.dto.ConcertDatesDto
 import dev.concert.application.concert.dto.ConcertsDto
 
 interface ConcertService {
     fun getConcerts(): List<ConcertsDto>
+    fun getAvailableDates(concertId: Long): List<ConcertDatesDto>
 }
