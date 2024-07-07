be6d6a14c44e4fc0d4d15645e46e6d831614b7c2
juhwan <asdcz@naver.com>
2024-07-07 23:56:36 +0900

    콘서트 좌석 조회 비즈니스로직 단위 테스트

--- src/test/kotlin/dev/concert/application/concert/service/ConcertServiceImplTest.kt
+++ src/test/kotlin/dev/concert/application/concert/service/ConcertServiceImplTest.kt
@@ -3,6 +3,8 @@ package dev.concert.application.concert.service
 import dev.concert.domain.ConcertRepository
 import dev.concert.domain.entity.ConcertEntity
 import dev.concert.domain.entity.ConcertOptionEntity
+import dev.concert.domain.entity.SeatEntity
+import dev.concert.domain.entity.status.SeatStatus
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -92,5 +94,45 @@ class ConcertServiceImplTest {
         assertEquals(2, availableDates.size)
         assertEquals("20241201", availableDates[0].concertDate)
         assertEquals("20241202", availableDates[1].concertDate)
+    }
+
+    @Test
+    fun `콘서트 예약 가능한 좌석을 조회한다`() {
+        // given
+        val concertOptionId = 1L
+        val concertOption = ConcertOptionEntity(
+            concert = ConcertEntity(
+                concertName = "새해 콘서트",
+                singer = "에스파",
+                startDate = "20241201",
+                endDate = "20241201",
+                reserveStartDate = "20241201",
+                reserveEndDate = "20241201"
+            ),
+            availableSeats = 50,
+            concertTime = "14:00",
+            concertVenue = "올림픽공원",
+            concertDate = "20241201"
+        )
+        val seats = listOf(
+            SeatEntity(
+                concertOption = concertOption,
+                seatNo = 1,
+                price = 100000,
+            ),
+            SeatEntity(
+                concertOption = concertOption,
+                seatNo = 2,
+                price = 100000,
+            )
+        )
+
+        given(concertRepository.getAvailableSeats(concertOptionId)).willReturn(seats)
+
+        // when
+        val availableSeats = concertService.getAvailableSeats(concertOptionId)
+
+        // then
+        assertEquals(2, availableSeats.size)
     }
 }
