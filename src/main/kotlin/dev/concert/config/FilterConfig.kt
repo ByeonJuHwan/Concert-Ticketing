5afa50cd08dbe7c113a3116467bb8f9080bd054e
juhwan <asdcz@naver.com>
2024-07-07 13:06:07 +0900

    토큰 infra layer 인터페이스 추가 with jpa
    토큰 검증 필터 추가
    
    토큰 정보 검색 비즈니스 로직 추가
    
    토큰 정보 조회 및 필터 검증 쿼리 추가
    
    토큰 정보 조회 컨트롤러 추가
    
    토큰 정보조회 단위 테스트 추가

---
+++ src/main/kotlin/dev/concert/config/FilterConfig.kt
@@ -0,0 +1,20 @@
+package dev.concert.config
+
+import dev.concert.presentation.filter.TokenValidateFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig (
+    private val tokenValidateFilter: TokenValidateFilter,
+) {
+
+    @Bean
+    fun tokenFilterRegistration(): FilterRegistrationBean<TokenValidateFilter> {
+        val registrationBean = FilterRegistrationBean<TokenValidateFilter>()
+        registrationBean.filter = tokenValidateFilter
+        registrationBean.addUrlPatterns("/queue/tokens/status")
+        return registrationBean
+    }
+}
