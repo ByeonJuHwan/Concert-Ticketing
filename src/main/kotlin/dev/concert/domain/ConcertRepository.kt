91588dbc862330d2221b0d54ec197877a10e72b9
juhwan <asdcz@naver.com>
2024-07-07 23:55:23 +0900

    콘서트 좌석 조회 infra layer 로직 인터페이스 구현

--- src/main/kotlin/dev/concert/domain/ConcertRepository.kt
+++ src/main/kotlin/dev/concert/domain/ConcertRepository.kt
@@ -2,8 +2,10 @@ package dev.concert.domain
 
 import dev.concert.domain.entity.ConcertEntity
 import dev.concert.domain.entity.ConcertOptionEntity
+import dev.concert.domain.entity.SeatEntity
 
 interface ConcertRepository {
     fun getConcerts(): List<ConcertEntity>
     fun getAvailableDates(concertId: Long): List<ConcertOptionEntity>
+    fun getAvailableSeats(concertOptionId: Long): List<SeatEntity>
 }
