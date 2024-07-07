a7502b5e30f2692568031a63a793689c28a85d0a
juhwan <asdcz@naver.com>
2024-07-07 17:32:57 +0900

    콘서트 리스트 조회 api 스웨거 어노테이션 추가

--- src/main/kotlin/dev/concert/presentation/ConcertController.kt
+++ src/main/kotlin/dev/concert/presentation/ConcertController.kt
@@ -2,17 +2,29 @@ package dev.concert.presentation
 
 import dev.concert.ApiResult
 import dev.concert.application.concert.facade.ConcertFacade
+import dev.concert.presentation.response.concert.ConcertAvailableDatesResponse
 import dev.concert.presentation.response.concert.ConcertsResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "콘서트 API", description = "콘서트 예약 API")
 @RestController
 @RequestMapping("/concerts")
 class ConcertController (
     private val concertFacade: ConcertFacade
 ) {
 
+
+    @Operation(summary = "콘서트 예약 가능 날짜 조회", description = "콘서트 예약 가능 날짜를 조회합니다.")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "콘서트 예약 가능 날짜 조회 성공"),
+    )
     @GetMapping
     fun getConcerts(): ApiResult<ConcertsResponse> {
         val concerts = concertFacade.getConcerts()
