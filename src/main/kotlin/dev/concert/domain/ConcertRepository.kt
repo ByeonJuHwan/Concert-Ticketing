ac4959339c451dbf0216ffd542a937e2f6aa572d
juhwan <asdcz@naver.com>
2024-07-07 20:15:24 +0900

    콘서트 날짜 조회 infra 로직 인터페이스 구현

--- src/main/kotlin/dev/concert/domain/ConcertRepository.kt
+++ src/main/kotlin/dev/concert/domain/ConcertRepository.kt
@@ -1,7 +1,9 @@
 package dev.concert.domain
 
 import dev.concert.domain.entity.ConcertEntity
+import dev.concert.domain.entity.ConcertOptionEntity
 
 interface ConcertRepository {
     fun getConcerts(): List<ConcertEntity>
+    fun getAvailableDates(concertId: Long): List<ConcertOptionEntity>
 }
