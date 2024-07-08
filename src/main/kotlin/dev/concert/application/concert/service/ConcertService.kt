14bd02ee67b0dc0e93927b0343df37a86970e8df
juhwan <asdcz@naver.com>
2024-07-08 21:02:04 +0900

    콘서트 좌석 예약 비즈니스로직 인터페이스 구현

--- src/main/kotlin/dev/concert/application/concert/service/ConcertService.kt
+++ src/main/kotlin/dev/concert/application/concert/service/ConcertService.kt
@@ -1,6 +1,8 @@
 package dev.concert.application.concert.service
 
 import dev.concert.application.concert.dto.ConcertDatesDto
+import dev.concert.application.concert.dto.ConcertReservationDto
+import dev.concert.application.concert.dto.ConcertReservationResponseDto
 import dev.concert.application.concert.dto.ConcertSeatsDto
 import dev.concert.application.concert.dto.ConcertsDto
 
@@ -8,4 +10,5 @@ interface ConcertService {
     fun getConcerts(): List<ConcertsDto>
     fun getAvailableDates(concertId: Long): List<ConcertDatesDto>
     fun getAvailableSeats(concertOptionId: Long): List<ConcertSeatsDto>
+    fun reserveSeat(request: ConcertReservationDto): ConcertReservationResponseDto
 }
