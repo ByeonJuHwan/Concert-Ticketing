c3f5abf7cee557fc9520f118d98f1453b45123d0
juhwan <asdcz@naver.com>
2024-07-10 00:17:11 +0900

    콘서트 서비스 비즈니스로직 간소화

--- src/main/kotlin/dev/concert/application/concert/service/ConcertService.kt
+++ src/main/kotlin/dev/concert/application/concert/service/ConcertService.kt
@@ -1,8 +1,6 @@
 package dev.concert.application.concert.service
 
 import dev.concert.application.concert.dto.ConcertDatesDto
-import dev.concert.application.concert.dto.ConcertReservationDto
-import dev.concert.application.concert.dto.ConcertReservationResponseDto
 import dev.concert.application.concert.dto.ConcertSeatsDto
 import dev.concert.application.concert.dto.ConcertsDto
 
@@ -10,5 +8,4 @@ interface ConcertService {
     fun getConcerts(): List<ConcertsDto>
     fun getAvailableDates(concertId: Long): List<ConcertDatesDto>
     fun getAvailableSeats(concertOptionId: Long): List<ConcertSeatsDto>
-    fun reserveSeat(request: ConcertReservationDto): ConcertReservationResponseDto
 }
