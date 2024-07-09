2918436d65f598721656a33345f0dac6f49c1e38
juhwan <asdcz@naver.com>
2024-07-10 00:17:45 +0900

    좌석 조회 및 상태 비즈니스 로직 인터페이스 설계

---
+++ src/main/kotlin/dev/concert/application/seat/SeatService.kt
@@ -0,0 +1,10 @@
+package dev.concert.application.seat
+
+import dev.concert.domain.entity.SeatEntity
+import dev.concert.domain.entity.status.SeatStatus
+
+interface SeatService {
+    fun getSeat (seatId: Long): SeatEntity
+    fun checkSeatAvailable(seat: SeatEntity)
+    fun changeSeatStatus(seat: SeatEntity, temporarilyAssigned: SeatStatus)
+}
