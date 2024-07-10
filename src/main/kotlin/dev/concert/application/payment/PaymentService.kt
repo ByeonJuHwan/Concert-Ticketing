61b97fc0d737c126ad74ade5dd2aafc47d3499b0
변주환 <asdcz@naver.com>
2024-07-10 17:22:56 +0900

    결제 비즈니스 로직 인터페이스 설계

--- src/main/kotlin/dev/concert/application/payment/PaymentService.kt
+++ src/main/kotlin/dev/concert/application/payment/PaymentService.kt
@@ -1,4 +1,7 @@
 package dev.concert.application.payment
 
+import dev.concert.domain.entity.ReservationEntity
+
 interface PaymentService {
+    fun createPayments(reservation: ReservationEntity)
 }
