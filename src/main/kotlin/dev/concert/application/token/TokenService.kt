a8345cd96a8ab8ff3852f4fc2dfd9dd6e94e1b8a
변주환 <asdcz@naver.com>
2024-07-10 17:23:28 +0900

    토큰 삭제 비즈니스 로직 인터페이스 설계

--- src/main/kotlin/dev/concert/application/token/TokenService.kt
+++ src/main/kotlin/dev/concert/application/token/TokenService.kt
@@ -1,9 +1,11 @@
 package dev.concert.application.token
 
 import dev.concert.application.token.dto.TokenResponseDto
+import dev.concert.domain.entity.UserEntity
 
 interface TokenService {
     fun generateToken(userId: Long): String
     fun isTokenAllowed(token: String): Boolean
     fun getToken(token: String): TokenResponseDto
+    fun deleteToken(user : UserEntity)
 }
