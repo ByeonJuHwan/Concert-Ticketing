9dbb8712d377766a41e8b5242a3a6882bbcd8918
변주환 <asdcz@naver.com>
2024-07-10 17:23:12 +0900

    예약 비즈니스 로직 인터페이스 설계

--- src/main/kotlin/dev/concert/application/reservation/ReservationService.kt
+++ src/main/kotlin/dev/concert/application/reservation/ReservationService.kt
@@ -3,7 +3,12 @@ package dev.concert.application.reservation
 import dev.concert.domain.entity.ReservationEntity
 import dev.concert.domain.entity.SeatEntity
 import dev.concert.domain.entity.UserEntity
+import dev.concert.domain.entity.status.ReservationStatus
+import java.time.LocalDateTime
 
 interface ReservationService {
     fun saveReservation(user : UserEntity, seat : SeatEntity) : ReservationEntity
+    fun getReservation(reservationId: Long): ReservationEntity
+    fun isExpired(reservation: ReservationEntity) : Boolean
+    fun changeReservationStatus(reservation: ReservationEntity, status: ReservationStatus)
 }
