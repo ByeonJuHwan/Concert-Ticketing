f45200bdb5a7da40588d30a4ab8026251b0badea
juhwan <asdcz@naver.com>
2024-07-11 00:26:12 +0900

    테스트 후 변경해야할 TODO 추가

--- src/main/kotlin/dev/concert/application/payment/PaymentFacade.kt
+++ src/main/kotlin/dev/concert/application/payment/PaymentFacade.kt
@@ -2,6 +2,7 @@ package dev.concert.application.payment
 
 import dev.concert.application.payment.dto.PaymentDto
 import dev.concert.application.payment.dto.PaymentResponseDto
+import dev.concert.application.point.service.PointHistoryService
 import dev.concert.application.point.service.PointService
 import dev.concert.application.reservation.ReservationService
 import dev.concert.application.token.TokenService
@@ -15,9 +16,11 @@ class PaymentFacade(
     private val reservationService: ReservationService,
     private val tokenService: TokenService,
     private val paymentService: PaymentService,
+    private val pointHistoryService: PointHistoryService,
 ) {
     fun pay(request: PaymentDto) : PaymentResponseDto {
         // 예약 정보를 가져온다
+        // TODO 여기서도 N+1 문제 발생 쿼리 처리 필요
         val reservation = reservationService.getReservation(request.reservationId)
 
         // 임시 좌석 저장인 5분 안에 결재 요청을 했는지 확인한다 expiresAt 으로 확인(만료되었다면 상태를 Expired로 변경하고 예외를 터트린다)
@@ -26,11 +29,15 @@ class PaymentFacade(
         }
 
         // 포인트 상태를 확인한다 (포인트가 price 보다 적으면 예외를 터트린다)
-        // TODO N+1 문제 발생 쿼리 처리 필요
         val currentPoint = pointService.checkPoint(reservation.user, reservation.seat.price)
 
         // 포인트 차감 -> 결제를 진행한다 (카드 무통장등등 많지만 요구사항은 포인트 이므로 포인트로 진행)
+        // TODO 실제 차감이 안됬음
         pointService.deductPoints(currentPoint, reservation.seat.price)
+
+        // 포인트 차감 히스토리 저장
+        // TODO 포인트 기록이 저장되게 변경
+        pointHistoryService.savePointHistory(reservation.user, reservation.seat.price)
 
         // 결재정보를 저장한다
         paymentService.createPayments(reservation)
