e3eb1ca8d541e7e6529731080cc8fd89f69a291b
변주환 <asdcz@naver.com>
2024-07-10 17:19:35 +0900

    포인트 확인 차감 비즈니스로직 인터페이스 생성

--- src/main/kotlin/dev/concert/application/point/service/PointService.kt
+++ src/main/kotlin/dev/concert/application/point/service/PointService.kt
@@ -1,9 +1,12 @@
 package dev.concert.application.point.service
 
 import dev.concert.application.point.dto.PointResponseDto
+import dev.concert.domain.entity.PointEntity
 import dev.concert.domain.entity.UserEntity
 
 interface PointService {
     fun chargePoints(user: UserEntity, amount: Long): PointResponseDto
     fun getCurrentPoint(user: UserEntity): PointResponseDto
+    fun checkPoint(user: UserEntity, price: Long) : PointEntity
+    fun deductPoints(currentPoint : PointEntity, price: Long)
 }
