2f7da77a011e72d43a5d48f00505a5f549171ff9
변주환 <asdcz@naver.com>
2024-07-10 17:15:31 +0900

    결제 요청 Dto 생성

---
+++ src/main/kotlin/dev/concert/application/payment/dto/PaymentDto.kt
@@ -0,0 +1,5 @@
+package dev.concert.application.payment.dto
+
+data class PaymentDto(
+    val reservationId : Long,
+)
--- src/main/kotlin/dev/concert/presentation/request/PaymentRequest.kt
+++ src/main/kotlin/dev/concert/presentation/request/PaymentRequest.kt
@@ -1,5 +1,11 @@
 package dev.concert.presentation.request
 
+import dev.concert.application.payment.dto.PaymentDto
+
 data class PaymentRequest(
     val reservationId : Long
+)
+
+fun PaymentRequest.toDto() = PaymentDto(
+    reservationId = reservationId,
 )
