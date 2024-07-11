7e436fe25d7be21ee4a80f972cd6928670564d58
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-07-12 02:01:59 +0900

    결제 비즈니스 로직 구현

--- src/main/kotlin/dev/concert/application/payment/PaymentServiceImpl.kt
+++ src/main/kotlin/dev/concert/application/payment/PaymentServiceImpl.kt
@@ -9,25 +9,25 @@ import dev.concert.exception.ReservationAlreadyPaidException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-@Service
-class PaymentServiceImpl(
-    private val paymentRepository : PaymentRepository
-) : PaymentService {
-
-    @Transactional
-    override fun createPayments(reservation: ReservationEntity) {
-        val payment = PaymentEntity(
-            reservation = reservation,
-            price = reservation.seat.price,
-            paymentStatus = PaymentStatus.SUCCESS,
-            paymentType = PaymentType.POINT
-        )
-        paymentRepository.save(payment)
-    }
-
-    override fun checkPayment(reservation: ReservationEntity) {
-        if(paymentRepository.existsByReservation(reservation)) {
-            throw ReservationAlreadyPaidException("이미 결제된 예약입니다.")
-        }
-    }
-}
+@Service 
+class PaymentServiceImpl( 
+    private val paymentRepository : PaymentRepository 
+) : PaymentService { 
+ 
+    @Transactional 
+    override fun createPayments(reservation: ReservationEntity) { 
+        val payment = PaymentEntity( 
+            reservation = reservation, 
+            price = reservation.seat.price, 
+            paymentStatus = PaymentStatus.SUCCESS, 
+            paymentType = PaymentType.POINT  
+        ) 
+        paymentRepository.save(payment) 
+    } 
+ 
+    override fun checkPayment(reservation: ReservationEntity) { 
+        if(paymentRepository.existsByReservation(reservation)) { 
+            throw ReservationAlreadyPaidException("이미 결제된 예약입니다.") 
+        } 
+    } 
+} 
