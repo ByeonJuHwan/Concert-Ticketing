b3db8b57fb0e14a6b4e4ef8aa776d1c61f6de97f
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-07-12 02:42:00 +0900

    대기열 토큰 관리 스케줄러

--- src/main/kotlin/dev/concert/util/TokenScheduler.kt
+++ src/main/kotlin/dev/concert/util/TokenScheduler.kt
@@ -4,16 +4,16 @@ import dev.concert.application.token.TokenFacade
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
-@Component
-class TokenScheduler (
-    private val tokenFacade: TokenFacade,
-) {
-
-    /**
-     * 토큰의 상태를 관리하는 스케줄러
-     */
-    @Scheduled(fixedRate = 60000) // 1분 마다 실행
-    fun tokenScheduler() {
-        tokenFacade.manageTokenStatus()
-    }
-}
+@Component 
+class TokenScheduler ( 
+    private val tokenFacade: TokenFacade, 
+) { 
+ 
+    /** 
+     * 토큰의 상태를 관리하는 스케줄러 
+     */ 
+    @Scheduled(fixedRate = 60000) // 1분 마다 실행 
+    fun tokenScheduler() { 
+        tokenFacade.manageTokenStatus() 
+    } 
+} 
