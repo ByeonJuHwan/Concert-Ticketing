67a3a67261bf1bf5f2454b6ded8a8bafb7febabb
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-07-12 01:30:52 +0900

    토큰 발급 조회 통합테스트 작성

--- src/test/kotlin/dev/concert/application/token/TokenIntegrationTest.kt
+++ src/test/kotlin/dev/concert/application/token/TokenIntegrationTest.kt
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
-@SpringBootTest
+@SpringBootTest 
 class TokenIntegrationTest {
 
     @Autowired
@@ -18,38 +18,38 @@ class TokenIntegrationTest {
     @Autowired
     private lateinit var userService: UserService
 
-    @Test
-    fun `userId 로 토큰을 암호화 하여 발급한다`() {
-        val user = userService.saveUser(UserEntity("변주환"))
+    @Test 
+    fun `userId 로 토큰을 암호화 하여 발급한다`() { 
+        val user = userService.saveUser(UserEntity("변주환")) 
+ 
+        // when 
+        val generateToken = tokenFacade.generateToken(user.id) 
+ 
+        // then 
+        assertThat(generateToken).isNotBlank() 
+    } 
 
-        // when
-        val generateToken = tokenFacade.generateToken(user.id)
+    @Test 
+    fun `발급된 토큰의 유효시간 보다 현재시간이 전이면 TRUE 를 반환한다`() { 
+        val user = userService.saveUser(UserEntity("변주환")) 
+        val token = tokenFacade.generateToken(user.id) 
+ 
+        // when 
+        val isTokenAllowed = tokenFacade.isTokenExpired(token) 
+ 
+        // then 
+        assertThat(isTokenAllowed).isFalse() 
+    } 
 
-        // then
-        assertThat(generateToken).isNotBlank()
-    }
-
-    @Test
-    fun `발급된 토큰의 유효시간 보다 현재시간이 전이면 TRUE 를 반환한다`() {
-        val user = userService.saveUser(UserEntity("변주환"))
-        val token = tokenFacade.generateToken(user.id)
-
-        // when
-        val isTokenAllowed = tokenFacade.isTokenExpired(token)
-
-        // then
-        assertThat(isTokenAllowed).isFalse()
-    }
-
-    @Test
-    fun `현재 토큰정보를 반환한다 `() {
-        val user = userService.saveUser(UserEntity("변주환"))
-        val token = tokenFacade.generateToken(user.id)
-
-        // when
-        val tokenResponseDto = tokenFacade.getToken(token)
-
-        // then
-        assertThat(tokenResponseDto.token).isEqualTo(token)
-    }
-}
+    @Test 
+    fun `현재 토큰정보를 반환한다 `() { 
+        val user = userService.saveUser(UserEntity("변주환")) 
+        val token = tokenFacade.generateToken(user.id) 
+ 
+        // when 
+        val tokenResponseDto = tokenFacade.getToken(token) 
+ 
+        // then 
+        assertThat(tokenResponseDto.token).isEqualTo(token) 
+    } 
+} 
