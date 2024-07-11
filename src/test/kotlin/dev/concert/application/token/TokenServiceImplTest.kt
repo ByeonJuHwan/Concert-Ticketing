6982e8b6c4facf3e9fbc475e5ebbd47826362169
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-07-12 01:30:07 +0900

    토큰 조회 발급 단위테스트 작성

--- src/test/kotlin/dev/concert/application/token/TokenServiceImplTest.kt
+++ src/test/kotlin/dev/concert/application/token/TokenServiceImplTest.kt
@@ -26,66 +26,66 @@ class TokenServiceImplTest {
     @InjectMocks
     lateinit var tokenServiceImpl: TokenServiceImpl
 
-    @Test
-    fun `특정 유저에 대해서 토큰이 생성 되어야 한다`() {
-        // given
-        val userId = 1L
-        val user = UserEntity(name = "test")
-        `when`(userRepository.findById(userId)).thenReturn(user)
+    @Test 
+    fun `특정 유저에 대해서 토큰이 생성 되어야 한다`() { 
+        // given 
+        val userId = 1L 
+        val user = UserEntity(name = "test") 
+        `when`(userRepository.findById(userId)).thenReturn(user) 
+ 
+        // when 
+        val token = tokenServiceImpl.generateToken(userId) 
+ 
+        // then 
+        assertNotNull(token) 
+    } 
 
-        // when
-        val token = tokenServiceImpl.generateToken(userId)
+    @Test 
+    fun `토큰이 만료되지 않았을 때 true를 반환해야 한다`() { 
+        // given 
+        val user = UserEntity(name = "test") 
+        val token = "test" 
+        val queueTokenEntity = QueueTokenEntity(token = token, user = user) 
+ 
+        `when`(tokenRepository.findByToken(token)).thenReturn(queueTokenEntity) 
+ 
+        // when 
+        val isTokenAllowed = tokenServiceImpl.isTokenExpired(token) 
+ 
+        // then 
+        assertFalse(isTokenAllowed) 
+    } 
 
-        // then
-        assertNotNull(token)
+    @Test 
+    fun `토큰이 주어지면 해당 토큰에 대한 정보를 반환해야 한다`() { 
+        // given 
+        val user = UserEntity(name = "test") 
+        val token = "test" 
+        val queueTokenEntity = QueueTokenEntity(token = token, user = user) 
+ 
+        `when`(tokenRepository.findByToken(token)).thenReturn(queueTokenEntity) 
+ 
+        // when 
+        val tokenResponseDto = tokenServiceImpl.getToken(token) 
+ 
+        // then 
+        assertNotNull(tokenResponseDto) 
+        assertThat(tokenResponseDto.token).isEqualTo(token) 
     }
 
-    @Test
-    fun `토큰이 만료되지 않았을 때 true를 반환해야 한다`() {
-        // given
-        val user = UserEntity(name = "test")
-        val token = "test"
-        val queueTokenEntity = QueueTokenEntity(token = token, user = user)
-
-        `when`(tokenRepository.findByToken(token)).thenReturn(queueTokenEntity)
-
-        // when
-        val isTokenAllowed = tokenServiceImpl.isTokenExpired(token)
-
-        // then
-        assertFalse(isTokenAllowed)
-    }
-
-    @Test
-    fun `토큰이 주어지면 해당 토큰에 대한 정보를 반환해야 한다`() {
-        // given
-        val user = UserEntity(name = "test")
-        val token = "test"
-        val queueTokenEntity = QueueTokenEntity(token = token, user = user)
-
-        `when`(tokenRepository.findByToken(token)).thenReturn(queueTokenEntity)
-
-        // when
-        val tokenResponseDto = tokenServiceImpl.getToken(token)
-
-        // then
-        assertNotNull(tokenResponseDto)
-        assertThat(tokenResponseDto.token).isEqualTo(token)
-    }
-
-    @Test
-    fun `토큰이 ACTIVE 상태인지 확인한다`() {
-        // given
-        val user = UserEntity(name = "test")
-        val token = "test"
-        val queueTokenEntity = QueueTokenEntity(token = token, user = user)
-
-        `when`(tokenRepository.findByToken(token)).thenReturn(queueTokenEntity)
-
-        // when
-        val isAvailableToken = tokenServiceImpl.isAvailableToken(token)
-
-        // then
-        assertFalse(isAvailableToken)
-    }
+    @Test 
+    fun `토큰이 ACTIVE 상태인지 확인한다`() { 
+        // given 
+        val user = UserEntity(name = "test") 
+        val token = "test" 
+        val queueTokenEntity = QueueTokenEntity(token = token, user = user) 
+ 
+        `when`(tokenRepository.findByToken(token)).thenReturn(queueTokenEntity) 
+ 
+        // when 
+        val isAvailableToken = tokenServiceImpl.isAvailableToken(token) 
+ 
+        // then 
+        assertFalse(isAvailableToken) 
+    } 
 }
