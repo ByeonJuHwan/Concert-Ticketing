6d22d182c98c56c577d877b17da0fc7daf2c4430
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-07-12 01:24:09 +0900

    토큰 Validate Filter 추가

--- src/main/kotlin/dev/concert/presentation/filter/TokenValidateFilter.kt
+++ src/main/kotlin/dev/concert/presentation/filter/TokenValidateFilter.kt
@@ -9,33 +9,32 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
-@Component
-class TokenValidateFilter (
-    private val tokenFacade: TokenFacade,
-) : OncePerRequestFilter() {
-
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-        val token = getBearerToken(request)
-        print("token: $token")
-        if (token == null) {
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "토큰이 없습니다")
-            return
-        }else if (tokenFacade.isTokenExpired(token)) {
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "토큰이 만료되었습니다")
-            return
-        } else if(!tokenFacade.isAvailableToken(token)){
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "대기열 순번 확인이 필요합니다")
-            return
-        }
-        filterChain.doFilter(request, response)
-    }
-
-    private fun getBearerToken(request: HttpServletRequest): String? {
-        val header = request.getHeader(HttpHeaders.AUTHORIZATION)
-        return if (header != null && header.startsWith("Bearer ")) {
-            header.substring(7)
-        } else {
-            null
-        }
-    }
-}
+@Component 
+class TokenValidateFilter ( 
+    private val tokenFacade: TokenFacade, 
+) : OncePerRequestFilter() { 
+ 
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) { 
+        val token = getBearerToken(request) 
+        if (token == null) { 
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "토큰이 없습니다") 
+            return 
+        }else if (tokenFacade.isTokenExpired(token)) { 
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "토큰이 만료되었습니다") 
+            return 
+        } else if(!tokenFacade.isAvailableToken(token)){ 
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "대기열 순번 확인이 필요합니다") 
+            return 
+        } 
+        filterChain.doFilter(request, response) 
+    } 
+ 
+    private fun getBearerToken(request: HttpServletRequest): String? { 
+        val header = request.getHeader(HttpHeaders.AUTHORIZATION) 
+        return if (header != null && header.startsWith("Bearer ")) { 
+            header.substring(7) 
+        } else { 
+            null 
+        } 
+    } 
+} 
