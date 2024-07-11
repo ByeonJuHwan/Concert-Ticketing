59539cd6aa0a074e82add1bc90a6f0ebd0d4a84e
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-07-12 01:26:01 +0900

    토큰 조회 발급 Facade 추가

--- src/main/kotlin/dev/concert/application/token/TokenFacade.kt
+++ src/main/kotlin/dev/concert/application/token/TokenFacade.kt
@@ -3,27 +3,27 @@ package dev.concert.application.token
 import dev.concert.application.token.dto.TokenResponseDto
 import org.springframework.stereotype.Service
 
-@Service
-class TokenFacade (
-    private val tokenService: TokenService,
-){
-    fun generateToken(userId: Long): String {
-        return tokenService.generateToken(userId)
-    }
+@Service 
+class TokenFacade ( 
+    private val tokenService: TokenService, 
+){ 
+    fun generateToken(userId: Long): String { 
+        return tokenService.generateToken(userId) 
+    } 
+ 
+    fun isTokenExpired(token: String): Boolean { 
+        return tokenService.isTokenExpired(token) 
+    } 
+ 
+    fun getToken(token: String): TokenResponseDto { 
+        return tokenService.getToken(token) 
+    } 
+ 
+    fun manageTokenStatus() { 
+        tokenService.manageTokenStatus() 
+    } 
 
-    fun isTokenExpired(token: String): Boolean {
-        return tokenService.isTokenExpired(token)
-    }
-
-    fun getToken(token: String): TokenResponseDto {
-        return tokenService.getToken(token)
-    }
-
-    fun manageTokenStatus() {
-        tokenService.manageTokenStatus()
-    }
-
-    fun isAvailableToken(token: String): Boolean {
-        return tokenService.isAvailableToken(token)
-    }
+    fun isAvailableToken(token: String): Boolean { 
+        return tokenService.isAvailableToken(token) 
+    } 
 }
