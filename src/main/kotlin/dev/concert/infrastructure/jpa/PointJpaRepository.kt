ae7b2a4ebbe5194d436bd9f86fef42b25b18cda6
juhwan <asdcz@naver.com>
2024-07-17 17:48:48 +0900

    페치조인으로 포인트 조회 N+1 해결

--- src/main/kotlin/dev/concert/infrastructure/jpa/PointJpaRepository.kt
+++ src/main/kotlin/dev/concert/infrastructure/jpa/PointJpaRepository.kt
@@ -2,15 +2,10 @@ package dev.concert.infrastructure.jpa
 
 import dev.concert.domain.entity.PointEntity
 import dev.concert.domain.entity.UserEntity
-import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 
 interface PointJpaRepository : JpaRepository<PointEntity, Long> {
+    @Query("select p from PointEntity p join fetch p.user where p.user = :user")
     fun findByUser(user : UserEntity) : PointEntity?
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select p from PointEntity p where p.user = :user")
-    fun findByUserWithLock(user : UserEntity) : PointEntity?
 }
