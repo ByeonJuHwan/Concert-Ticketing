595d22e51ed7208bab3a6bfc99d0fa494ea9fcbd
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-08-01 20:50:18 +0900

    레디스 인프라스트럭쳐 구현체 구현

--- src/main/kotlin/dev/concert/infrastructure/redis/TokenRedisRepositoryImpl.kt
+++ src/main/kotlin/dev/concert/infrastructure/redis/TokenRedisRepositoryImpl.kt
@@ -7,54 +7,54 @@ import dev.concert.domain.util.consts.WAITING_QUEUE
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
 import java.util.concurrent.TimeUnit
+ 
+@Repository 
+class TokenRedisRepositoryImpl( 
+    private val redisTemplate: RedisTemplate<String, String>, 
+) : QueueTokenRepository { 
+ 
+    override fun findTokenByKey(key: String): String? { 
+        return redisTemplate.opsForValue().get(key) 
+    } 
+ 
+    override fun addWaitingQueue(userJson: String, currentTime: Double) { 
+        redisTemplate.opsForZSet().add(WAITING_QUEUE, userJson, currentTime) 
+    } 
+ 
+    override fun createToken(userKey: String, token: String) { 
+        redisTemplate.opsForValue().set(userKey, token, 1, TimeUnit.HOURS) 
+    } 
 
-@Repository
-class TokenRedisRepositoryImpl(
-    private val redisTemplate: RedisTemplate<String, String>,
-) : QueueTokenRepository {
+    override fun getTokenExpireTime(userKey: String): Long? { 
+        return redisTemplate.getExpire(userKey, TimeUnit.SECONDS) 
+    } 
 
-    override fun findTokenByKey(key: String): String? {
-        return redisTemplate.opsForValue().get(key)
-    }
+    override fun deleteTokenActiveQueue(token: String) { 
+        redisTemplate.opsForSet().remove(ACTIVE_QUEUE, token) 
+    } 
 
-    override fun addWaitingQueue(userJson: String, currentTime: Double) {
-        redisTemplate.opsForZSet().add(WAITING_QUEUE, userJson, currentTime)
-    }
+    override fun deleteToken(userKey: String) { 
+        redisTemplate.delete(userKey) 
+    } 
 
-    override fun createToken(userKey: String, token: String) {
-        redisTemplate.opsForValue().set(userKey, token, 1, TimeUnit.HOURS)
-    }
+    override fun findTopWaitingTokens(start: Long, end: Long): Set<String>? { 
+        return redisTemplate.opsForZSet().range(WAITING_QUEUE, start, end) 
+    } 
 
-    override fun getTokenExpireTime(userKey: String): Long? {
-        return redisTemplate.getExpire(userKey, TimeUnit.SECONDS)
-    }
+    override fun addActiveQueue(token: String) { 
+        redisTemplate.opsForSet().add(ACTIVE_QUEUE, token) 
+    } 
 
-    override fun deleteTokenActiveQueue(token: String) {
-        redisTemplate.opsForSet().remove(ACTIVE_QUEUE, token)
-    }
+    override fun removeWaitingQueueToken(userJson: String) { 
+        redisTemplate.opsForZSet().remove(WAITING_QUEUE, userJson) 
+    } 
 
-    override fun deleteToken(userKey: String) {
-        redisTemplate.delete(userKey)
-    }
+    override fun isTokenInActiveQueue(userJson: String): Boolean { 
+        return redisTemplate.opsForSet().isMember(ACTIVE_QUEUE, userJson) ?: false 
+    } 
 
-    override fun findTopWaitingTokens(start: Long, end: Long): Set<String>? {
-        return redisTemplate.opsForZSet().range(WAITING_QUEUE, start, end)
-    }
-
-    override fun addActiveQueue(token: String) {
-        redisTemplate.opsForSet().add(ACTIVE_QUEUE, token)
-    }
-
-    override fun removeWaitingQueueToken(userJson: String) {
-        redisTemplate.opsForZSet().remove(WAITING_QUEUE, userJson)
-    }
-
-    override fun isTokenInActiveQueue(userJson: String): Boolean {
-        return redisTemplate.opsForSet().isMember(ACTIVE_QUEUE, userJson) ?: false
-    }
-
-    override fun getRankInWaitingQueue(userJson: String): Long? {
-        return redisTemplate.opsForZSet().rank(WAITING_QUEUE, userJson)
-    }
+    override fun getRankInWaitingQueue(userJson: String): Long? { 
+        return redisTemplate.opsForZSet().rank(WAITING_QUEUE, userJson) 
+    } 
 
 }
