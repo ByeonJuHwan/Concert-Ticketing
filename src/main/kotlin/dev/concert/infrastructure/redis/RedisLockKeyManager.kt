e93a0938ef2421396d4145fc093eab0e53fd1fd4
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-08-01 20:50:37 +0900

    레디스 락 키 구현체 구현

--- src/main/kotlin/dev/concert/infrastructure/redis/RedisLockKeyManager.kt
+++ src/main/kotlin/dev/concert/infrastructure/redis/RedisLockKeyManager.kt
@@ -2,10 +2,10 @@ package dev.concert.infrastructure.redis
 
 import dev.concert.domain.util.lock.LockKeyGenerator
 import org.springframework.stereotype.Component
-
-@Component
-class RedisLockKeyManager : LockKeyGenerator {
-    override fun generateLockKeyWithPrefix(prefix: String, param: Any): String {
-        return "$prefix:$param"
-    }
-}
+ 
+@Component 
+class RedisLockKeyManager : LockKeyGenerator { 
+    override fun generateLockKeyWithPrefix(prefix: String, param: Any): String { 
+        return "$prefix:$param" 
+    } 
+} 
