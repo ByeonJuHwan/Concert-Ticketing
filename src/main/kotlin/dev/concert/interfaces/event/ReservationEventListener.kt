1f7d5de549eba5133db2e603ef9f76ce3aa59cd3
juhwan <asdcz@naver.com>
2024-08-07 21:25:12 +0900

    [이벤트 설계] 예외발생시 예외 로깅 추가

--- src/main/kotlin/dev/concert/interfaces/event/ReservationEventListener.kt
+++ src/main/kotlin/dev/concert/interfaces/event/ReservationEventListener.kt
@@ -2,6 +2,8 @@ package dev.concert.interfaces.event
 
 import dev.concert.application.data.DataPlatformFacade
 import dev.concert.domain.service.reservation.event.ReservationSuccessEvent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
@@ -12,9 +14,16 @@ class ReservationEventListener (
     private val dataPlatformFacade: DataPlatformFacade,
 ) {
 
+    private val log : Logger = LoggerFactory.getLogger(ReservationEventListener::class.java)
+
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handleExternalApiEvent(event: ReservationSuccessEvent) {
-        dataPlatformFacade.sendReservationData(event.reservationId)
+        runCatching {
+            dataPlatformFacade.sendReservationData(event.reservationId)
+        }.onFailure { ex ->
+            // 예외 처리 로직
+            log.error("데이터 플랫폼 전송 에러 : ${ex.message}", ex)
+        }
     }
 }
