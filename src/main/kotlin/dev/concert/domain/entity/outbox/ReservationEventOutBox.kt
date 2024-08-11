e076b67abfb70d709b34496a02c47def9ec828fc
juhwan <asdcz@naver.com>
2024-08-11 22:44:39 +0900

    [아웃박스패턴] 아웃박스 테이블 생성

---
+++ src/main/kotlin/dev/concert/domain/entity/outbox/ReservationEventOutBox.kt
@@ -0,0 +1,25 @@
+package dev.concert.domain.entity.outbox
+
+import dev.concert.domain.entity.status.OutBoxMsgStats
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class ReservationEventOutBox(
+    reservationId: Long,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long = 0
+
+    var reservationId : Long = reservationId
+        protected set
+
+    @Enumerated(EnumType.STRING)
+    var status : OutBoxMsgStats = OutBoxMsgStats.INIT
+        protected set
+}
