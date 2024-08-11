4f332e51774e93ca0b7345fb7b4ce6052120c464
juhwan <asdcz@naver.com>
2024-08-11 14:47:45 +0900

    applicationEvent, kafkaEvent 둘다 사용가능하게 변경

--- src/main/kotlin/dev/concert/domain/event/reservation/ReservationEvent.kt
+++ src/main/kotlin/dev/concert/domain/event/reservation/ReservationEvent.kt
@@ -1,3 +1,11 @@
 package dev.concert.domain.event.reservation
 
-interface ReservationEvent
+interface ReservationEvent {
+    fun toKafkaMessage() : String
+}
+
+data class ReservationSuccessEvent(val reservationId: Long) : ReservationEvent {
+    override fun toKafkaMessage() : String{
+        return reservationId.toString()
+    }
+}
