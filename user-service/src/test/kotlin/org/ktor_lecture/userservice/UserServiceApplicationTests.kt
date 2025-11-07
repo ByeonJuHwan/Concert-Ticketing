c5c5eddc72afb27bc1526c32b12d6279faae73fe
ByeonJuHwan <asdcz@naver.com>
2025-11-07 14:31:40 +0900

    유저 서비스 추가

---
+++ user-service/src/test/kotlin/org/ktor_lecture/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ktor_lecture.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
