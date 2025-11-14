0dee20d428171648d6174e27bb17264fcac68daf
ByeonJuHwan <asdcz@naver.com>
2025-11-14 11:21:54 +0900

    service 주석 추가

--- user-service/src/main/kotlin/org/ktor_lecture/userservice/adapter/in/event/UserEventListener.kt
+++ user-service/src/main/kotlin/org/ktor_lecture/userservice/adapter/in/event/UserEventListener.kt
@@ -14,11 +14,19 @@ class UserEventListener (
     private val sendUserCreatedEventUseCase: SendUserCreatedEventUseCase,
 ) {
 
+    /**
+     * [아웃박스 패턴]
+     * BEFORE_COMMIT 으로 실행되어 어떤 이벤트가 발행되어야 하는지 저장한다
+     */
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     fun handleUserCreatedEvent(event: UserCreatedEvent) {
         createUserCreateOutBoxUseCase.recordUserCreatedOutBoxMsg(event)
     }
 
+    /**
+     * [아웃박스 패턴]
+     * AFTER_COMMIT 시 카프카 이벤트를 발행한다
+     */
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun publishUseCreatedEvent(event: UserCreatedEvent) {
         sendUserCreatedEventUseCase.publishUseCreatedEvent(event)
