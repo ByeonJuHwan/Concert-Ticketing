39faa3f5760fe7e29ec8c4df45595ddfc35ac0e3
ByeonJuHwan <asdcz@naver.com>
2025-11-15 12:38:30 +0900

    token-service 추가

---
+++ token-service/src/test/kotlin/org/ktor_lecture/tokenservice/TokenServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ktor_lecture.tokenservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TokenServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
