59dc2f3797aecadf42ec7d6e5f1b8c1d2aa07755
ByeonJuHwan <asdcz@naver.com>
2025-11-18 10:21:48 +0900

    포인트 충전 이력 저장 로직 추가

---
+++ user-service/src/main/kotlin/org/ktor_lecture/userservice/domain/entity/PointHistoryEntity.kt
@@ -0,0 +1,40 @@
+package org.ktor_lecture.userservice.domain.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.ConstraintMode
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "point_history")
+class PointHistoryEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val user: UserEntity,
+
+    @Column(nullable = false)
+    val amount: Long,
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val type: PointTransactionType,
+) : BaseEntity() {
+}
+
+enum class PointTransactionType {
+    CHARGE,
+    USE,
+}
