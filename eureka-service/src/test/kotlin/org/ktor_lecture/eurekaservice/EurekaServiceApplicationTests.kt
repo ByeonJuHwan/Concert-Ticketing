a5584d0dbcbf9d8706b6eb8878590416a93edd7d
ByeonJuHwan <asdcz@naver.com>
2026-02-03 13:04:34 +0900

    eurekaServer 추가

---
+++ eureka-service/src/test/kotlin/org/ktor_lecture/eurekaservice/EurekaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ktor_lecture.eurekaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
